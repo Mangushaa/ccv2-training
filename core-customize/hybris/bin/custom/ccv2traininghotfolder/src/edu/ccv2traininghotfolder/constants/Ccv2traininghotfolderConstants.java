8b373ce25d34d1ce59a41fa8e2fd3e56f0bb4632
Marko_Popyk <marko.popyk@cantire.com>
2024-05-17 02:36:49 +0300

    Added hotfolder product sync logic.

---
+++ core-customize/hybris/bin/custom/ccv2traininghotfolder/src/edu/ccv2traininghotfolder/constants/Ccv2traininghotfolderConstants.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package edu.ccv2traininghotfolder.constants;
+
+/**
+ * Global class for all Ccv2traininghotfolder constants. You can add global constants for your extension into this class.
+ */
+public final class Ccv2traininghotfolderConstants extends GeneratedCcv2traininghotfolderConstants
+{
+	public static final String EXTENSIONNAME = "ccv2traininghotfolder";
+
+	private Ccv2traininghotfolderConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+
+	public static final String PLATFORM_LOGO_CODE = "ccv2traininghotfolderPlatformLogo";
+}
